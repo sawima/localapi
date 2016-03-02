196cdad036661a4d8faa518614045230e2efc233
James Hong <James Hong>
2016-03-02 12:22:30 +0800

    work on sync , need promise to get token

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var express = require('express'),
     fs = require('fs'),
     path = require('path'),
     logger = require('morgan'),
-    http = require('http');
+    http = require('http'),
+    qs = require('querystring');
 
 var app =  express(),
     apirouter=express.Router(),
@@ -14,18 +15,16 @@ var schedule = require('node-schedule');
 
 var tokenSchema = new Schema({
   token:String
+// },{
+//   capped:{
+//     size:4096,
+//     max:1
+//   }
 });
 
 mongoose.model('Token', tokenSchema);
 
-mongoose.model('Store',new Schema({
-  storeId:String,
-  storeCode: String,
-  storeName:String,
-  storeNameCN:String
-}));
-
-mongoose.model('Coupon',new Schema({
+var CouponSchema ={
   name:String,
   details:[{
     depositNum:Number, 
@@ -37,12 +36,27 @@ mongoose.model('Coupon',new Schema({
     type:String,
     enum:['newmember','deposit']
   }
+};
+
+mongoose.model('Store',new Schema({
+  storeId:String,
+  storeCode: String,
+  storeName:String,
+  storeNameCN:String,
+  newMemberCoupon:CouponSchema,
+  depositCoupon:CouponSchema
+// },{
+//   capped:{
+//     size:4096,
+//     max:1
+//   }
 }));
+
+
 
 var Token=mongoose.model('Token');
 
 var Store=mongoose.model('Store');
-var Coupon=mongoose.model('Coupon');
 
 
 var j = schedule.scheduleJob(config.scheduleTokenStr, function(){
@@ -51,14 +65,14 @@ var j = schedule.scheduleJob(config.scheduleTokenStr, function(){
   // http.get()
 
   var postData = JSON.stringify({
-  'account' : 'sq',
-  'password' : 'password'
+  'account' : config.account,
+  'password' : config.password
   });
 
   var options = {
     hostname: config.centralServer,
     port: config.centralServerPort,
-    path: config.centralServerPath,
+    path: config.centralServerAuthPath,
     method: 'POST',
     headers: {
       // 'Content-Type': 'application/x-www-form-urlencoded',
@@ -92,6 +106,65 @@ var j = schedule.scheduleJob(config.scheduleTokenStr, function(){
   req.end();
 
 });
+
+//sync store and coupons
+var jsync = schedule.scheduleJob(config.scheduleStoreStr, function(){
+  // console.log('The answer to life, the universe, and everything!');
+  //here is the cron task to update token
+  // http.get()
+
+  var postData = JSON.stringify({
+  'account' : config.account,
+  'password' : config.password
+  });
+
+  //todo: promise get token
+  var token='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1NmM3ZDQyNjFiYjY4YWIwMzBhNDczMDEiLCJzdG9yZUNvZGUiOiJDTlNISjAwMSIsInN0b3JlTmFtZSI6IlJpdmVyIiwic3RvcmVOYW1lQ04iOiLmiZHlk6fot68iLCJzdG9yZVR5cGUiOiJEaXJlY3QiLCJsb2MiOltdLCJpYXQiOjE0NTY4ODk3MzcsImV4cCI6MTQ1Njk3NjEzN30.046URuZeS481yYWaf4F7MlnEJ2HwcvsemmKvFd4SS2E';
+
+  var options = {
+    hostname: config.centralServer,
+    port: config.centralServerPort,
+    path: config.centralServerSyncPath+"/?token="+token,
+    method: 'POST',
+    // query:qs.stringify({token:token}),
+    headers: {
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json',
+      'Content-Length': postData.length
+    }
+  };
+
+  var req = http.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      // console.log(`BODY: ${chunk}`);
+      //save data to db
+      // console.log(chunk);
+      var getdata=JSON.parse(chunk);
+
+      console.log("(((((((((((((((((((((((((((((((((((((");
+      console.log(getdata.store);
+      var store = new Store(getdata.store);
+      // var coupons = new Store(getdata.store);
+      store.save();
+    });
+    res.on('end', () => {
+      console.log('No more data in response.')
+    })
+  });
+
+  req.on('error', (e) => {
+    console.log(`problem with request: ${e.message}`);
+  });
+
+  req.write(postData);
+  req.end();
+
+});
+
+
 
 // mongoose.connect("mongodb://127.0.0.1:27017/mpstoken",{safe:true});
 mongoose.connect(config.db,{safe:true});
--- config/config.js
+++ config/config.js
@@ -1,13 +1,14 @@
 var config=module.exports={};
 
 config.db=process.env.TOKEN_MONGO_URL || "mongodb://127.0.0.1:27017/mpstoken";
-config.account="sq";
-config.password="password";
+config.account="CNSHJ001";
+config.password="luis2x1or";
 config.centralServer='127.0.0.1';
 config.centralServerPort=3000;
-config.centralServerPath="/api/auth";
+config.centralServerAuthPath="/api/auth";
+config.centralServerSyncPath="/api/sync";
 config.scheduleTokenStr='1 1 * * *';
-config.scheduleStoreStr='*/10 * * * *';
+config.scheduleStoreStr='*/1 * * * *';
 // config.updateIn=43200;
 
 
