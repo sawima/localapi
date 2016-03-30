3b5e5e3e7628e98da2293ca44e28ccee16cbcafd
James Hong <James Hong>
2016-03-30 16:46:27 +0800

    events change to picture

--- config/routers.js
+++ config/routers.js
@@ -18,26 +18,31 @@ module.exports =  function(router) {
   router.route('/getEvents').get(function(req,res) {
     // Events.findOne(function(err,token) {
     //   if(err) throw err;
-      var events=[{
-        title:"换季积分活动",
-        description:"here is the events details",
-        rules:[
-          "会员到店洗衣并大桶烘干30分钟1次即积1分。",
-          "每积5分，即可兑换一张14kg洗衣加14kg烘干30分钟免费券。",
-          "获赠洗烘有效期：2016年6月15日"
-        ],
-        timeperoid:"2016年3月1日至4月30日",
-        qrcode:"https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3851013351,2699800874&fm=58",
-        picture:"http://db.kimacloud.com/images/store.jpg"
-      },
-      {
-        title:"‘推荐亲友，畅洗我衣’奖赏活动",
-        description:"2016年春季来临，速比坤自助洗衣店酬宾活动现推出：会员推荐亲友，奖励丰富！",
-        rules:["活动期间，现有会员只要推荐亲友成为新会员，并充值300元，即可与亲友一起获得免费洗烘券各一张，推荐越多，奖励越多！同时，充值300元即可送50元。"],
-        timeperoid:"2016年3月1日至4月30日",
-        qrcode:"https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3851013351,2699800874&fm=58",
-        picture:"http://db.kimacloud.com/images/store.jpg"
-      }]
+      // var events=[{
+      //   title:"换季积分活动",
+      //   description:"here is the events details",
+      //   rules:[
+      //     "会员到店洗衣并大桶烘干30分钟1次即积1分。",
+      //     "每积5分，即可兑换一张14kg洗衣加14kg烘干30分钟免费券。",
+      //     "获赠洗烘有效期：2016年6月15日"
+      //   ],
+      //   timeperoid:"2016年3月1日至4月30日",
+      //   qrcode:"https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3851013351,2699800874&fm=58",
+      //   picture:"http://db.kimacloud.com/images/store.jpg"
+      // },
+      // {
+      //   title:"‘推荐亲友，畅洗我衣’奖赏活动",
+      //   description:"2016年春季来临，速比坤自助洗衣店酬宾活动现推出：会员推荐亲友，奖励丰富！",
+      //   rules:["活动期间，现有会员只要推荐亲友成为新会员，并充值300元，即可与亲友一起获得免费洗烘券各一张，推荐越多，奖励越多！同时，充值300元即可送50元。"],
+      //   timeperoid:"2016年3月1日至4月30日",
+      //   qrcode:"https://ss0.baidu.com/6ONWsjip0QIZ8tyhnq/it/u=3851013351,2699800874&fm=58",
+      //   picture:"http://db.kimacloud.com/images/store.jpg"
+      // }]
+      var events=[
+        "http://db.kimacloud.com/images/newstore.png",
+        "http://db.kimacloud.com/images/softwash.png",
+        "http://db.kimacloud.com/images/family.png"
+      ]
       res.json({success:true,message:"get latest events",data:{events:events}}); 
     // });
   });
