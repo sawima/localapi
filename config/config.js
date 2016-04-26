3f82e99000fb4d059009252cb82a4f93fba9ae9e
James Hong <James Hong>
2016-04-26 10:21:39 +0800

    chagne env to mpsonline

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 var config=module.exports={};
 
 config.db=process.env.TOKEN_MONGO_URL || "mongodb://127.0.0.1:27017/mpstoken";
-config.account="CNSHMH001";
-config.password="0n41dtb7qfr";
-config.centralServer='db.kimacloud.com';
+config.account="shjdbridge";
+config.password="r6z2ztcsor";
+config.centralServer='mpsonline.sqstore.net';
 config.centralServerPort=80;
 config.centralServerAuthPath="/api/auth";
 config.centralServerSyncPath="/api/sync";
